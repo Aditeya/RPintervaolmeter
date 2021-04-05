76a7939b65834a117d69473fba34ed9139c3bb3b
aditeya <22963960+Aditeya@users.noreply.github.com>
2021-04-05 19:33:26 +0400

    turned timer into a class

---
+++ timer.py
@@ -0,0 +1,33 @@
+import os
+from sense_hat import SenseHat
+from time import sleep
+
+red   = [255,   0,  0]
+green = [  0, 255,  0]
+blue  = [  0,   0,255]
+
+class Timer:
+    def __init__(self):
+        self._running  = True
+    
+    def terminate(self):
+        self._running = False
+    
+    def run(self, s, sense):
+        while True:
+            timer(s, sense)
+
+def timer(s, sense):
+    display = [blue for i in range(64)]
+    sense.set_pixels(display)
+    
+    for i in range(0, 64):
+        sleep(s/64)
+        display[i] = green
+        sense.set_pixels(display)
+    
+    os.system("gphoto2 --keep")
+    sense.clear(red)
+    sleep(0.1)
+    sense.clear()
+
